885c1758cb1901aa4796e47704b553ee99869844
Beau <beau.williams@my.jcu.edu.au>
2017-03-24 10:29:13 +1000

    add temperatures.py to repository

---
+++ temperatures.py
@@ -0,0 +1,27 @@
+def main():
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            c_to_f = celsius_to_fahrenheit(celsius)
+            print(c_to_f)
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            f_to_c = fahrenheit_to_celsius(fahrenheit)
+            print(f_to_c)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+main()
